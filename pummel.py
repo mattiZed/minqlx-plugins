1a472f1ca450bb2b7c976361d1c80dbaa302629e
Stefan <stefan.matlage@googlemail.com>
2016-01-02 19:07:49 +0100

    Initial commit

---
+++ pummel.py
@@ -0,0 +1,51 @@
+# Copyright (C) mattiZed (github) aka mattiZed (ql)
+
+# This is a fun plugin written for Mino's Quake Live Server Mod minqlx
+# It displays "Killer x:y Victim" message when Victim gets killed with gauntlet
+# and stores the information in REDIS DB
+
+# Players can display their "pummels" via !pummel - this works only for victims
+# that are on the server on the same time, Otherwise we could just spit out
+# steamIDs.
+
+import minqlx
+
+# DB related
+PLAYER_KEY = "minqlx:players:{}"
+
+class pummel(minqlx.Plugin):
+    def __init__(self):
+        self.add_hook("kill", self.handle_kill)
+        
+        self.add_command("pummel", self.cmd_pummel)
+        
+    def handle_kill( self, victim, killer, data ):
+        if data["MOD"] == "GAUNTLET" and self.game.state == "in_progress":
+            self.play_sound("sound/vo_evil/humiliation1")
+            
+            self.db.sadd(PLAYER_KEY.format(killer.steam_id) + ":pummeled", str(victim.steam_id) )
+            self.db.incr(PLAYER_KEY.format(killer.steam_id) + ":pummeled:" + str(victim.steam_id))
+    
+            killer_score = self.db[PLAYER_KEY.format(killer.steam_id) + ":pummeled:" + str(victim.steam_id)]
+            victim_score = 0
+            if PLAYER_KEY.format(victim.steam_id) + ":pummeled:" + str(killer.steam_id) in self.db:
+                victim_score = self.db[PLAYER_KEY.format(victim.steam_id) + ":pummeled:" + str(killer.steam_id)]
+            
+            msg = "^1PUMMEL!^7 {} ^1{}^7:^1{}^7 {}".format( killer.name, killer_score, victim_score, victim.name )
+            self.msg( msg )
+    
+    def cmd_pummel( self, player, msg, channel ):
+        pummels = self.db.smembers( PLAYER_KEY.format(player.steam_id) + ":pummeled" )
+        players = self.teams()["spectator"] + self.teams()["red"] + self.teams()["blue"]
+        
+        msg = ""
+        for p in pummels:
+            for pl in players:
+                if p == str(pl.steam_id):
+                    count = self.db[PLAYER_KEY.format(player.steam_id) + ":pummeled:" + p]
+                    msg +=  pl.name + ": ^1" + count + "^7 "
+        if msg == "":
+            self.msg( "{} has not pummeled anybody on this server.".format(player) )
+        else:
+            self.msg( "^1NAKED ^7DB >> Pummel Stats for {}:".format(player) )
+            self.msg( msg )
